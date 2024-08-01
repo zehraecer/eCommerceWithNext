423a394bbf194df0b67f2135694e2ff094602bc3
Zehra Ecer <zehraecer1996@hotmail.com>
2024-08-01 18:42:56 +0300

    homepage eklendi

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HomePage from "./homePage/page";
 
 export default function Home() {
   return (
     <>
-
+      <HomePage />
     </>
   );
 }
